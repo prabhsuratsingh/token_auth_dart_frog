0f703562abe7c864b42c21b927be6cb4ac684bf5
prabhsuratsingh <prabhsurat17@gmail.com>
2024-03-02 13:35:30 +0530

    Implemented token check
    only checks if token exist, no validation check

--- routes/auth.dart
+++ routes/auth.dart
@@ -1,6 +1,24 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
-  // TODO: implement route handler
-  return Response(body: 'This is a new route!');
+Future<Response> onRequest(RequestContext context) async {
+  
+  var request = context.request;
+  var header = request.headers['authorization'];
+
+  //check if token is provided or not
+  if(header == null) {
+    return Response.json(
+      statusCode: HttpStatus.unauthorized,
+      body: {'Token Status':'No Token', 'auth': 'not authorized'},
+      );
+  }
+  else {
+    return Response.json(
+      statusCode: HttpStatus.ok,
+      body: {'Token Status':'Token Exists','auth':'Authorized'},
+      );
+  }
+  
 }
